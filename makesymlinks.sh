db47cecdb328327c9b4aaf33c22cfb6d02ca01c5
Calle Kabo <calle.kabo@gmail.com>
2016-02-08 22:26:40 +0100

    urxvt stuff and tmux

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files="vimrc vim gvimrc"
+files="vimrc vim gvimrc Xresources Xresources.d tmux.conf"
 for file in $files; do
 				echo "Symlinking ${file}..."
 				ln -sf ~/dotfiles/${file} ~/.${file}
